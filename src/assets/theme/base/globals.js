682027dbac7169f909a7d98006a9a96575fb9d92
kittycodes <77451065+lekhanakraj@users.noreply.github.com>
2023-09-28 20:17:27 +0530

    global css

--- src/assets/theme/base/globals.js
+++ src/assets/theme/base/globals.js
@@ -18,7 +18,7 @@
 
 // Vision UI Dashboard React Base Styles
 import colors from "assets/theme/base/colors";
-import bgAdmin from "assets/images/body-background-w.jpg";
+import bgAdmin from "assets/images/body-background-w2.jpg";
 
 const { info, dark } = colors;
 export default {
